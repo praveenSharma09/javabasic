09e9375e9ded6ae9f9f42a8374346b7ed098a8ff
praveenSharma09 <golugolgopaldass@gmail.com>
2024-10-09 12:38:38 +0530

    patterns

---
+++ loops.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+import java.util.*;
+public class loops {
+    public static void main(String args[]){
+        int n  = 4;
+       // patterns
+       for( int i = 1; i<=n; i++){
+           for(int j = 1;  j<=i; j++){
+               System.out.print("* ");
+
+           }
+           System.out.println();
+
+       }
+
+
+    }
+
+
+
+}
