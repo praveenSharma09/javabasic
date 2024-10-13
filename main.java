d7c30c651a9dc5def99aff4a4ff31f5cd2700ae8
praveenSharma09 <golugolgopaldass@gmail.com>
2024-10-14 01:04:36 +0530

    dash

--- main.java
+++ main.java
@@ -1,7 +1,20 @@
+import java.sql.SQLOutput;
+import java.util.*;
 class Test
 {
     public static void main(String []args)
     {
         System.out.println("My First Java Program.");
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i<=10; i++){
+            int result =  N*i;
+            System.out.println(N+ " * " + i + " = " +  result);
+
+        }
+        System.out.println();
+        System.out.println("thanks");
     }
+
 }
