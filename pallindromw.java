48f64094c5d1fa0ec868158ea2ec8b0fb7e276d2
praveenSharma09 <golugolgopaldass@gmail.com>
2024-10-15 03:23:02 +0530

    doom

---
+++ pallindromw.java
@@ -0,0 +1,23 @@
+public class pallindromw {
+
+
+        public static void main(String[] args) {
+
+            String A = "madam";
+            /* Enter your code here. Print output to STDOUT. */
+            System.out.println(pallin(A));
+        }
+        static boolean pallin(String A){
+
+
+            for(int i = 0; i<A.length()/2; i++){
+                char start  = A.charAt(i);
+                char end  = A.charAt(A.length()-1-i);
+                if(start != end){
+                    return false;}
+
+            }
+            return true;
+        }
+    }
+
